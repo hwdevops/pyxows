23f49f0584b395df2b8903d96d51a244aab3f16c
hwdevops <hwieger.devops@gmail.com>
2021-11-16 08:38:39 +0100

    [INTERNAL] added connection error handling

--- websocket-start.py
+++ websocket-start.py
@@ -2,6 +2,7 @@
 https://community.cisco.com/t5/collaboration-voice-and-video/xapi-over-websocket-xows-ce9-7-x/ba-p/3831553
 """
 
+from os import error
 import websockets
 import ssl
 import asyncio
@@ -10,8 +11,12 @@ import base64
 import env
 
 async def connect():
-    return await websockets.connect('wss://{}/ws/'.format(env.ip_address_webex_board), ssl=ssl._create_unverified_context(), extra_headers={
-        'Authorization': 'Basic {}'.format(base64.b64encode('{}:{}'.format(env.ce_username, env.ce_password).encode()).decode('utf-8'))})
+    try:
+        return await websockets.connect('wss://{}/ws/'.format(env.ip_address_dx80), ssl=ssl._create_unverified_context(), extra_headers={'Authorization': 'Basic {}'.format(base64.b64encode('{}:{}'.format(env.ce_username, env.ce_password).encode()).decode('utf-8'))})
+    except websockets.exceptions.InvalidStatusCode as e:
+        print(f"ERROR: {e}: On CE Device set NetworkServices > Websocket to FollowHTTPService")
+    except Exception as e:
+        print(e)
 
 async def send(ws, message):
     await ws.send(message)
@@ -23,10 +28,11 @@ async def receive(ws):
 
 async def task():
     ws = await connect()
-    try:
-        await send(ws, '{"jsonrpc": "2.0","id": "0","method": "xGet","params": {"Path": ["Status", "SystemUnit", "State"]}}')
-        await receive(ws)
-    finally:
-        ws.close()
+    if ws:
+        try:
+            await send(ws, '{"jsonrpc": "2.0","id": "0","method": "xGet","params": {"Path": ["Status", "SystemUnit", "State"]}}')
+            await receive(ws)
+        finally:
+            ws.close()
 
 asyncio.run(task())
